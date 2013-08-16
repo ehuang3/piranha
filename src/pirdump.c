9fb8ca4c87c76d29dfda3ec894427e6bf679c7dc
Neil T. Dantam <ntd@gatech.edu>
2013-08-16 19:30:08 -0400

    Add state dumping program

---
+++ src/pirdump.c
@@ -0,0 +1,98 @@
+/* -*- mode: C; c-basic-offset: 4  -*- */
+/* ex: set shiftwidth=4 expandtab: */
+/*
+ * Copyright (c) 2013, Georgia Tech Research Corporation
+ * All rights reserved.
+ *
+ * Author(s): Neil T. Dantam <ntd@gatech.edu>
+ * Georgia Tech Humanoid Robotics Lab
+ * Under Direction of Prof. Mike Stilman <mstilman@cc.gatech.edu>
+ *
+ *
+ * This file is provided under the following "BSD-style" License:
+ *
+ *
+ *   Redistribution and use in source and binary forms, with or
+ *   without modification, are permitted provided that the following
+ *   conditions are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *
+ *   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+ *   CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+ *   INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ *   MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ *   DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ *   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ *   LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ *   USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ *   AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *   LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *   ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *   POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
+/** Author: Neil Dantam
+ */
+
+#include <argp.h>
+#include <syslog.h>
+#include <sns.h>
+#include <signal.h>
+#include <unistd.h>
+#include <inttypes.h>
+#include <gamepad.h>
+#include <amino.h>
+#include <reflex.h>
+#include "piranha.h"
+
+void dump(struct pir_state *state);
+
+int main( int argc, char **argv ) {
+
+    ach_channel_t chan;
+    sns_start();
+
+    // open channel
+    sns_chan_open( &chan,   "pir-state",   NULL );
+    {
+        ach_channel_t *chans[] = {&chan, NULL};
+        sns_sigcancel( chans, sns_sig_term_default );
+    }
+
+
+    // state
+    /* -- RUN -- */
+    while (!sns_cx.shutdown) {
+        struct pir_state state;
+        size_t frame_size;
+        ach_status_t r = ach_get( &chan, &state, sizeof(state), &frame_size,
+                                  NULL, ACH_O_LAST );
+        switch(r) {
+        case ACH_OK:
+        case ACH_MISSED_FRAME:
+            dump(&state);
+        case ACH_CANCELED:
+        case ACH_TIMEOUT:
+        case ACH_STALE_FRAMES:
+            break;
+        default:
+            SNS_LOG(LOG_ERR, "Failed to get frame: %s\n", ach_result_to_string(r) );
+        }
+    }
+}
+
+void dump(struct pir_state *state) {
+    double ql[4], xl[3];
+    aa_tf_duqu2qv( state->S_L, ql, xl );
+    printf("xl: "); aa_dump_vec( stdout, xl, 3 );
+
+}
