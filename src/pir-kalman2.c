af220b7b83ac835f29c53c6d1b0ba90dfe2e0051
Neil T. Dantam <ntd@gatech.edu>
2014-07-06 00:02:00 -0400

    pir-kalman2 draft

--- src/pir-kalman2.c
+++ src/pir-kalman2.c
@@ -57,6 +57,9 @@ size_t opt_k = 1;
 size_t *opt_fixed_markers = NULL;
 size_t opt_n_fixed_markers = 0;
 
+size_t opt_n_cam = 0;
+char **opt_cam = NULL;
+
 #define N_MAX 8
 
 //#define MK0 (32+5)
@@ -125,12 +128,6 @@ int marker2frame( size_t marker_id ) {
     }
 }
 
-
-rfx_tf_dx XX_true;
-rfx_tf_dx XX_est;
-rfx_tf_dx ZZ;
-rfx_tf_dx UU;
-
 struct madqg_state {
     double E[7];
     double dx[6];
@@ -142,12 +139,28 @@ struct madqg_state {
     double P[13*13];
 };
 
+void init_state( struct madqg_state *state, size_t k ) {
+    AA_MEM_CPY( state->E, aa_tf_quat_ident, 4 );
+    AA_MEM_ZERO( state->E+4, 3 );
+    AA_MEM_ZERO( state->dx, 6 );
+    state->delta_theta = AA_NEW0_AR( double, k );
+    state->delta_x = AA_NEW0_AR( double, k );
+    state->n = 0;
+    state->max = 0;
+    state->i = 0;
+    aa_la_diag( 13, state->P, 1 );
+}
+
 struct madqg_state *state_bEc; ///< camera poses
 struct madqg_state *state_bEm; ///< fixed marker poses
 struct madqg_state state_bEr;  ///< right-hand kinematic pose
 struct madqg_state state_bEl;  ///< left-hand kinematic pose
 struct madqg_state state_rErp; ///< right-hand correction
 struct madqg_state state_lElp; ///< left-hand correction
+
+
+double state_tf_rel[7*PIR_TF_FRAME_MAX];
+double state_tf_abs[7*PIR_TF_FRAME_MAX];
 
 double Pb[13*13] = {0};
 double Wb[7*7] = {0};
@@ -156,65 +169,6 @@ double Vb[13*13] = {0};
 #define REC_RAW 0
 #define REC_MED (REC_RAW+4)
 #define REC_KF (REC_MED+1)
-
-struct cor_samp {
-    double bEk[7*N_MAX];
-    double cEo[7*N_MAX];
-    size_t n;
-};
-
-struct cor_samp *get_samp( ach_channel_t *chan_config,
-                           ach_channel_t *chan_marker,
-                           struct sns_msg_wt_tf **_wt_tf,
-                           double **_tf_abs )
-{
-    struct cor_samp *samp = AA_MEM_REGION_LOCAL_NEW( struct cor_samp );
-    // get marker
-    struct sns_msg_wt_tf *wt_tf;
-    {
-        size_t marker_frame_size;
-        ach_status_t r = sns_msg_wt_tf_local_get( chan_marker, &wt_tf,
-                                                  &marker_frame_size, NULL, ACH_O_WAIT | ACH_O_LAST );
-        SNS_REQUIRE( r == ACH_OK || r == ACH_MISSED_FRAME,
-                     "Error getting markers: %s\n", ach_result_to_string(r) );
-        SNS_REQUIRE( 0 == sns_msg_wt_tf_check_size(wt_tf, marker_frame_size),
-                     "Invalid wt_tf message size: %lu \n", marker_frame_size );
-    }
-    // get config
-    double *config;
-    {
-        size_t frame_size;
-        ach_status_t r = sns_msg_local_get( chan_config, (void**)&config,
-                                            &frame_size, NULL, ACH_O_WAIT | ACH_O_LAST );
-        SNS_REQUIRE( r == ACH_OK || r == ACH_MISSED_FRAME,
-                     "Error getting config: %s\n", ach_result_to_string(r) );
-        size_t expected_size = 2*PIR_TF_CONFIG_MAX*sizeof(double);
-        SNS_REQUIRE( expected_size == frame_size,
-                     "Unexpected frame size: saw %lu, wanted %lu\n",
-                     frame_size, expected_size );
-    }
-
-    // get kinematics
-    double *tf_rel = AA_MEM_REGION_LOCAL_NEW_N( double, 7*PIR_TF_FRAME_MAX );
-    double *tf_abs = AA_MEM_REGION_LOCAL_NEW_N( double, 7*PIR_TF_FRAME_MAX );
-    pir_tf_rel( config, tf_rel );
-    pir_tf_abs( tf_rel, tf_abs );
-
-    // get correspondences
-    samp->n = 0;
-    for( size_t j = 0; j < wt_tf->header.n; j ++ ) {
-        ssize_t frame = marker2frame(j);
-        if( frame < 0 || wt_tf->wt_tf[j].weight < opt_wt_thresh ) continue;
-        assert( samp->n < N_MAX );
-        AA_MEM_CPY( AA_MATCOL(samp->bEk, 7, samp->n), AA_MATCOL(tf_abs,7,frame), 7 );
-        AA_MEM_CPY( AA_MATCOL(samp->cEo, 7, samp->n), wt_tf->wt_tf[j].tf.data, 7 );
-        samp->n++;
-    }
-
-    *_wt_tf = wt_tf;
-    *_tf_abs = tf_abs;
-    return samp;
-}
 
 int correct2( struct madqg_state *state, size_t n_obs,
                const double *bEm, const double *bEc )
@@ -237,6 +191,11 @@ int correct1( struct madqg_state *state, const double *E_obs )
                                  state->P, Wb );
 }
 
+int predict( double dt, struct madqg_state *state )
+{
+    return rfx_lqg_qutr_predict( dt, state->E, state->dx, state->P, Vb );
+}
+
 void update_camera( double *tf_abs, size_t i_cam, struct sns_msg_wt_tf *wt_tf )
 {
     // Can initialze camera pose off of arm position, since we know
@@ -247,6 +206,7 @@ void update_camera( double *tf_abs, size_t i_cam, struct sns_msg_wt_tf *wt_tf )
     double cor_cam[7*16];
     size_t i_cor = 0;
 
+    // TODO: E.E. relative
     for( size_t j = 0; j < wt_tf->header.n; j ++ ) {
         ssize_t frame = marker2frame(j);
         if( wt_tf->wt_tf[j].weight < opt_wt_thresh ) continue;
@@ -275,57 +235,119 @@ void update_camera( double *tf_abs, size_t i_cam, struct sns_msg_wt_tf *wt_tf )
     correct2( state_bEc+i_cam, i_cor, cor_body, cor_cam );
 }
 
-void update( ach_channel_t *chan_config )
+void update( ach_channel_t *chan_config, ach_channel_t *chan_cam, size_t n_cam )
 {
     /**** KINEMATICS ****/
     /* Wait Sample */
-    double *config;
+    double *config = NULL;
+    ach_status_t r_config;
     {
         size_t frame_size;
-        ach_status_t r = sns_msg_local_get( chan_config, (void**)&config,
-                                            &frame_size, NULL, ACH_O_WAIT | ACH_O_LAST );
-        SNS_REQUIRE( r == ACH_OK || r == ACH_MISSED_FRAME,
-                     "Error getting config: %s\n", ach_result_to_string(r) );
-        size_t expected_size = 2*PIR_TF_CONFIG_MAX*sizeof(double);
-        SNS_REQUIRE( expected_size == frame_size,
-                     "Unexpected frame size: saw %lu, wanted %lu\n",
-                     frame_size, expected_size );
+        r_config = sns_msg_local_get( chan_config, (void**)&config,
+                                      &frame_size, NULL, ACH_O_WAIT | ACH_O_LAST );
+        switch( r_config ) {
+        case ACH_OK:
+        case ACH_MISSED_FRAME:
+        case ACH_STALE_FRAMES:
+            break;
+        default:
+            SNS_DIE( "Error getting config: %s\n", ach_result_to_string(r_config) );
+        }
+        /* Maybe update kinematics */
+        if( config ) {
+            size_t expected_size = 2*PIR_TF_CONFIG_MAX*sizeof(double);
+            SNS_REQUIRE( expected_size == frame_size,
+                         "Unexpected frame size: saw %lu, wanted %lu\n",
+                         frame_size, expected_size );
+            pir_tf_rel( config, state_tf_rel );
+            pir_tf_abs( state_tf_rel, state_tf_abs );
+            AA_MEM_CPY( state_bEl.E, state_tf_abs+7*PIR_TF_LEFT_WRIST0,7 );
+            AA_MEM_CPY( state_bEr.E, state_tf_abs+7*PIR_TF_LEFT_WRIST0,7 );
+        }
     }
-    /* Maybe update kinematics */
-    double *tf_rel = AA_MEM_REGION_LOCAL_NEW_N( double, 7*PIR_TF_FRAME_MAX );
-    double *tf_abs = AA_MEM_REGION_LOCAL_NEW_N( double, 7*PIR_TF_FRAME_MAX );
-    pir_tf_rel( config, tf_rel );
-    pir_tf_abs( tf_rel, tf_abs );
-    AA_MEM_CPY( state_bEl.E, tf_abs+7*PIR_TF_LEFT_WRIST0,7 );
-    AA_MEM_CPY( state_bEr.E, tf_abs+7*PIR_TF_LEFT_WRIST0,7 );
 
     /* PREDICT */
     double dt = 1e-1; // FIXME
-    rfx_lqg_qutr_predict( dt, XX_est.tf.data, XX_est.dx.data, Pb, Vb );
+    for( size_t i = 0; i < n_cam; i ++ ) {
+        predict( dt, state_bEc );
+    }
+    for( size_t i = 0; i < opt_n_fixed_markers; i ++ ) {
+        predict( dt, state_bEm );
+    }
+    predict( dt, &state_bEr );
+    predict( dt, &state_bEl );
+    predict( dt, &state_rErp );
+    predict( dt, &state_lElp );
 
     /**** CAMERAS ****/
     /* Get Sample */
-
-    /* CORRECT */
+    for( size_t i = 0; i < n_cam; i ++ ) {
+        struct sns_msg_wt_tf *wt_tf = NULL;
+        /* Get message */
+        size_t marker_frame_size;
+        ach_status_t r = sns_msg_wt_tf_local_get( &chan_cam[i], &wt_tf,
+                                                  &marker_frame_size, NULL, ACH_O_WAIT | ACH_O_LAST );
+        switch( r ) {
+        case ACH_OK:
+        case ACH_MISSED_FRAME:
+        case ACH_STALE_FRAMES:
+            break;
+        default:
+            SNS_DIE( "Error getting marker: %s\n", ach_result_to_string(r) );
+        }
+        /* CORRECT */
+        if( wt_tf ) {
+            SNS_REQUIRE( 0 == sns_msg_wt_tf_check_size(wt_tf, marker_frame_size),
+                         "Invalid wt_tf message size: %lu \n", marker_frame_size );
+            update_camera( state_tf_abs, i, wt_tf );
+        }
+    }
 
     /* CLEANUP */
     aa_mem_region_local_release();
 }
 
+
+int output( ach_channel_t *chan_reg ) {
+    struct sns_msg_tf *tfmsg = sns_msg_tf_local_alloc( (uint32_t)(2 + opt_n_cam + opt_n_fixed_markers ));
+    struct timespec now;
+
+    clock_gettime( ACH_DEFAULT_CLOCK, &now );
+    sns_msg_set_time( &tfmsg->header, &now, 0 );
+
+    AA_MEM_CPY( tfmsg->tf[0].data, state_bEl.E, 7 );
+    AA_MEM_CPY( tfmsg->tf[1].data, state_bEr.E, 7 );
+    for( size_t i = 0; i < opt_n_cam; i ++ )
+        AA_MEM_CPY( tfmsg->tf[2+i].data, state_bEc[i].E, 7 );
+    for( size_t i = 0; i < opt_n_fixed_markers; i ++ )
+        AA_MEM_CPY( tfmsg->tf[2+opt_n_cam+i].data, state_bEm[i].E, 7 );
+
+    enum ach_status r = sns_msg_tf_put(chan_reg, tfmsg);
+    SNS_REQUIRE( r == ACH_OK, "Couldn't put message\n");
+    return 0;
+}
+
+
 int main( int argc, char **argv )
 {
     sns_init();
     /* Parse */
-    for( int c; -1 != (c = getopt(argc, argv, "?k:" )); ) {
+    for( int c; -1 != (c = getopt(argc, argv, "?k:c:" )); ) {
         switch(c) {
         case 'k':
             opt_k = (size_t) atoi(optarg);
+            break;
+        case 'c':
+            opt_cam = (char**)realloc( opt_cam, (sizeof(char*)) * ++opt_n_cam );
+            opt_cam[opt_n_cam-1] = optarg;
             break;
         case '?':   /* help     */
             puts( "Usage: pir-kalman\n"
                   "Kalman filter for piranha arm\n"
                   "\n"
                   "Options:\n"
+                  "  -k number          MAD windown count\n"
+                  "  -c channel         Channel to read markers from\n"
                   "  -?,                Help text\n"
                   "\n"
                   "Report bugs to <ntd@gatech.edu>"
@@ -341,118 +363,39 @@ int main( int argc, char **argv )
     SNS_REQUIRE( opt_k > 0, "Must have positive sample count" );
 
     // init
-    ach_channel_t chan_config, chan_marker, chan_reg, chan_reg_rec;
+    ach_channel_t chan_config, *chan_marker, chan_reg, chan_reg_rec;
     sns_chan_open( &chan_config, "pir-config", NULL );
-    sns_chan_open( &chan_marker, "markers", NULL );
-    sns_chan_open( &chan_reg, "pir-reg", NULL );
+    sns_chan_open( &chan_reg, "pir-reg2", NULL );
     sns_chan_open( &chan_reg_rec, "pir-reg-rec", NULL );
+
+    chan_marker = (ach_channel_t*)malloc( sizeof(ach_channel_t*) * opt_n_cam );
+    for( size_t i = 0; i < opt_n_cam; i ++ ) {
+        sns_chan_open( &chan_marker[i], opt_cam[i], NULL );
+    }
+
     {
         ach_channel_t *chans[] = {&chan_reg, NULL};
         sns_sigcancel( chans, sns_sig_term_default );
     }
 
-    struct cor_samp *prev_samps = (struct cor_samp*)malloc(sizeof(struct cor_samp) *(size_t) opt_k);
-    AA_MEM_ZERO( prev_samps, opt_k );
+    for( size_t i = 0; i < opt_n_cam; i ++ ) {
+        init_state( &state_bEc[i], opt_k );
+    }
 
-    aa_la_diag( 13, Pb, 1 );
-
-    memset(&XX_true,0,sizeof(XX_true));
-    memset(&XX_est,0,sizeof(XX_est));
-    memset(&ZZ,0,sizeof(ZZ));
-    memset(&UU,0,sizeof(UU));
-    printf("foo\n");
+    for( size_t i = 0; i < opt_n_fixed_markers; i ++ ) {
+        init_state( &state_bEm[i], opt_k );
+    }
+    init_state( &state_bEl, opt_k );
+    init_state( &state_bEr, opt_k );
+    init_state( &state_lElp, opt_k );
+    init_state( &state_rErp, opt_k );
 
     // run
     while( !sns_cx.shutdown ) {
 
-        // sample
-        double *tf_abs;
-        struct sns_msg_wt_tf *wt_tf;
-        struct cor_samp *samp = get_samp(&chan_config, &chan_marker, &wt_tf, &tf_abs);
+        update( &chan_config, chan_marker, opt_n_cam );
+        output( &chan_reg );
 
-        // shift into previous
-        for( size_t i = 1; i < opt_k; i ++ ) {
-            prev_samps[i-1] = prev_samps[i];
-        }
-        prev_samps[opt_k-1] = *samp;
-
-        // collect data
-        double *X = AA_MEM_REGION_LOCAL_NEW_N(double,7*opt_k*N_MAX);
-        double *Y = AA_MEM_REGION_LOCAL_NEW_N(double,7*opt_k*N_MAX);
-        size_t n = 0;
-        for( size_t i = 0; i < opt_k; i++ ) {
-            for( size_t j = 0; j < prev_samps[i].n; j++ ) {
-                AA_MEM_CPY( AA_MATCOL(X,7,n), prev_samps[i].bEk, 7 );
-                AA_MEM_CPY( AA_MATCOL(Y,7,n), prev_samps[i].cEo, 7 );
-                n++;
-            }
-        }
-
-        if( 0 == n ) {
-            printf("none\n");
-            // no markers
-            continue;
-        }
-
-        // compute correspondence
-        double E_cor[7];
-        rfx_tf_cor( RFX_TF_COR_O_ROT_MEDIAN | RFX_TF_COR_O_TRANS_MEDIAN,
-                    n,
-                    X, 7, X+4, 7,
-                    Y, 7, Y+4, 7,
-                    E_cor );
-
-        //aa_tf_qminimize(E_cor);
-
-        // filter correspondence
-        AA_MEM_CPY( ZZ.tf.data, E_cor, 7 );
-        static int first = 1;
-        if( first ) {
-            first = 0;
-            AA_MEM_CPY( XX_est.tf.data, E_cor, 7 );
-        }
-        double dt = 1e-1;
-        rfx_lqg_qutr_process_noise( dt, 1, 1, XX_est.tf.data, Vb );
-        aa_la_diag( 7, Wb, dt*1e1 );
-
-        rfx_lqg_qutr_predict( dt, XX_est.tf.data, XX_est.dx.data, Pb, Vb );
-
-        rfx_lqg_qutr_correct( 1,
-                              XX_est.tf.data, XX_est.dx.data,
-                              ZZ.tf.data,
-                              Pb, Wb );
-
-        // send message
-        {
-            struct sns_msg_tf *tfmsg = sns_msg_tf_local_alloc(2);
-            struct timespec now;
-            clock_gettime( ACH_DEFAULT_CLOCK, &now );
-            sns_msg_set_time( &tfmsg->header, &now, 0 );
-            AA_MEM_CPY( tfmsg->tf[0].data, E_cor, 7 );
-            AA_MEM_CPY( tfmsg->tf[1].data, XX_est.tf.data, 7 );
-            enum ach_status r = sns_msg_tf_put(&chan_reg, tfmsg);
-            SNS_REQUIRE( r == ACH_OK, "Couldn't put message\n");
-        }
-
-
-        // extra message
-        {
-            struct sns_msg_tf *tfmsg = sns_msg_tf_local_alloc(6);
-            struct timespec now;
-            clock_gettime( ACH_DEFAULT_CLOCK, &now );
-            sns_msg_set_time( &tfmsg->header, &now, 0 );
-            size_t j[4] = {MK_R_L0M, MK_R_L1M, MK_R_R0P, MK_R_R1P};
-            for( size_t i = 0; i < 4; i ++ ) {
-                size_t frame = (size_t)marker2frame(j[i]);
-                aa_tf_qutr_mulc( AA_MATCOL(tf_abs,7,frame),
-                                 wt_tf->wt_tf[j[i]].tf.data,
-                                 tfmsg->tf[REC_RAW+i].data );
-            }
-            AA_MEM_CPY( tfmsg->tf[REC_MED].data, E_cor, 7 );
-            AA_MEM_CPY( tfmsg->tf[REC_KF].data, XX_est.tf.data, 7 );
-            enum ach_status r = sns_msg_tf_put(&chan_reg_rec, tfmsg);
-            SNS_REQUIRE( r == ACH_OK, "Couldn't put message\n");
-        }
         aa_mem_region_local_release();
     }
 }
